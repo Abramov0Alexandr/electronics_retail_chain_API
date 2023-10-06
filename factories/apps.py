a60c250624e091196ec9b715574ef5bb77fdfbf5
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-10-07 00:18:24 +0300

    Создал необходимые для проекта приложения

---
+++ factories/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class FactoriesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'factories'
+    verbose_name = 'Заводы'
