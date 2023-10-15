bc99c14bf80e3c49871870225e77cd51169850fa
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-10-15 18:19:39 +0300

    Написал сигналы для асинхронного обновления связанной информации

--- suppliers/signals.py
+++ suppliers/signals.py
@@ -1,7 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from rest_framework import serializers, status
-
 from contacts.models import Contacts
 from suppliers.models import RetailChains, Vendors
 
